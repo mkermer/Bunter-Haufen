990271675683e1d2c060298e7b99a652d3eed4f6
Morgane Kermer <kermer.morgane@gmail.com>
2021-12-15 00:55:44 +0100

    develop branch

--- bunter-haufen/src/App.js
+++ bunter-haufen/src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import Home from '././components/Home/Home.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <Home/>
       </header>
     </div>
