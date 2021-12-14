1604bfd8e1a96dc6f2074e05cf1925eab869b8c1
Morgane Kermer <kermer.morgane@gmail.com>
2021-12-15 00:14:29 +0100

    Hello World

---
+++ bunter-haufen/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HelloWorld = () => {
+
+    return (
+        <div className="content">
+            <h1>Hello World</h1>
+        </div>
+    )
+}
+
+
+export default HelloWorld;
